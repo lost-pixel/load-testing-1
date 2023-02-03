4ccb91d5465ad5dc4f7b178582a78d27f30764bf
d-ivashchuk <dimaiv@me.com>
2023-02-03 20:53:50 +0100

    switch to prod

--- lostpixel.config.js
+++ lostpixel.config.js
@@ -4,6 +4,6 @@ module.exports = {
   },
   lostPixelProjectId: "cldir11o10003lm0eqr1lgfgj",
   apiKey: process.env.LOST_PIXEL_API_KEY,
-  lostPixelPlatform: "https://api.staging.lost-pixel.com",
+  lostPixelPlatform: "https://api.lost-pixel.com",
   // lostPixelPlatform: "https://api.tmp.lost-pixel.com",
 };
