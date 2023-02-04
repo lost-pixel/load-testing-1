b7fd97710e8c80c050f732adab4f8e7ae9ea40ed
d-ivashchuk <dimaiv@me.com>
2023-02-04 15:23:58 +0100

    switch to staging env

--- lostpixel.config.js
+++ lostpixel.config.js
@@ -2,7 +2,7 @@ module.exports = {
   customShots: {
     currentShotsPath: "./lost-pixel",
   },
-  lostPixelProjectId: "cldoy8exl010df00emzaf0bz8",
+  lostPixelProjectId: "cldir11o10003lm0eqr1lgfgj",
   apiKey: process.env.LOST_PIXEL_API_KEY,
   lostPixelPlatform: "https://api.staging.lost-pixel.com",
 };
