1c780bab7c5ff992be6ddebfb4184a06882a6483
d-ivashchuk <dimaiv@me.com>
2023-02-10 10:10:03 +0100

    add images

--- lostpixel.config.js
+++ lostpixel.config.js
@@ -2,7 +2,7 @@ module.exports = {
   customShots: {
     currentShotsPath: "./lost-pixel",
   },
-  lostPixelProjectId: "cldir11o10003lm0eqr1lgfgj",
+  lostPixelProjectId: "",
   apiKey: process.env.LOST_PIXEL_API_KEY,
   lostPixelPlatform: "https://api.staging.lost-pixel.com",
 };
